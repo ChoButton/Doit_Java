0577c22aa257d37ebe003cfe8f43d94aac9aac78
bitcamp1\bitcamp <chosy0716@gmail.com>
2023-05-08 19:58:28 +0900

    뷰어기준 140P까지 공부

---
+++ chapter05/src/classpart/_01/Person.java
@@ -0,0 +1,9 @@
+package classpart._01;
+
+public class Person {
+	String name;
+	int height;
+	double weight;
+	char gender;
+	boolean married;
+}
---
+++ chapter05/src/classpart/_01/Student.java
@@ -0,0 +1,12 @@
+package classpart._01;
+
+public class Student {
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + " , " + address);
+	}
+}
